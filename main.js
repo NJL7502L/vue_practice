91a7caf38667cb37a3003a681e801aec7d3ea7b7
NJL7502L <g.hana1211.tako@gmail.com>
2020-07-07 17:13:03 +0900

    オブジェクトのネスト

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el:'#app',
     data:{
+        message:{
+            greeting:"Hello Vue.js!",
+            fruitList:["リンゴ","バナナ","みかん"],
+            num:1
+        },
         show:true
     }
 })
